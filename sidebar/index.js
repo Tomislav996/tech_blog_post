a9788d3cfdc83fff991657b55c5c050a51645ed6
Tomislav996 <trpeskitomislav@outlook.com>
2023-06-02 19:17:00 +0200

    popup newsletter added

--- sidebar/index.js
+++ sidebar/index.js
@@ -1,9 +1,25 @@
 let btn = document.querySelector('#sidebarCollapse');
 let sidebar = document.querySelector('#sidebar');
 let btnIcon = document.querySelector('#btn-icon')
+let modalButtons = document.querySelectorAll('.modal_button');
 
 
 btn.onclick = function () {
     sidebar.classList.toggle('active');
 }
 
+let isOtherModalClicked = false;
+
+
+modalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    isOtherModalClicked = true;
+  })
+})
+
+setTimeout(function() {
+  if(!isOtherModalClicked) {
+    let myModal = new bootstrap.Modal(document.getElementById('modal-popup'));
+    myModal.show();
+  }
+}, 1000);
